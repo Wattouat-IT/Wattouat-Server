08cbd81a095c81c8a93abe0e1c117e550ddfc998
czantoine <ac11f@free.fr>
2022-02-22 08:53:05 +0100

    Update server_info.sh
    add id in the conf file

--- fonction/mysql/server_info.sh
+++ fonction/mysql/server_info.sh
@@ -17,3 +17,7 @@ day_time=$(uptime | sed 's/.*up//')
 day_time=$(echo $day_time | sed 's/,.*//')
 
 mysql -u admin -proot elisa -e "INSERT INTO node_info (host_name, os, ip, date_add, up_time, day_time)  VALUES ('Server Wattouat : $host_name', '$os', '$ip_host', NOW(), '$up_time', '$day_time');"
+id_node_info=$(mysql -u admin -proot elisa -h $ip -e "SELECT MAX( id ) FROM node_info;" | sed 's/[^0-9]//g')
+id_node_info=$(echo $id_node_info)
+echo "# ID of this Server into elisa database" >> /bin/wattouat/wattouat.conf
+echo "id_server_info=$id_node_info" >> /bin/wattouat/wattouat.conf
