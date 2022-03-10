cc79aeb5519c49dcc5c6feb9b00202ff573b248e
czantoine <ac11f@free.fr>
2022-03-10 08:44:09 +0100

    Update data_dump.sql

--- data_dump.sql
+++ data_dump.sql
@@ -16,7 +16,7 @@ CREATE TABLE cpu(
   cpu_model VARCHAR(50),
   cpu_mhz INT(50),
   cpu_core INT(50),
-  id_node_info INT,
+  id_node_info INT NOT NULL,
   PRIMARY KEY(id)
 );
 
@@ -42,72 +42,63 @@ CREATE TABLE kwh(
   PRIMARY KEY(id)
 );
 
-/* Relation 'node_info_list_service' */
-ALTER TABLE cpu
-  ADD CONSTRAINT node_info_list_service
-    FOREIGN KEY (id_node_info) REFERENCES node_info (id);
-
-/* Relation 'cpu_kwh' */
-ALTER TABLE kwh ADD CONSTRAINT cpu_kwh FOREIGN KEY (cpu_id) REFERENCES cpu (id);
-/* Table 'node_info' */
-CREATE TABLE node_info(
+/* Table 'service' */
+CREATE TABLE service(
   id INT NOT NULL AUTO_INCREMENT,
-  host_name VARCHAR(30),
-  os VARCHAR(30),
-  ip VARCHAR(20),
-  `date_add` DATETIME,
-  up_time DATE,
-  day_time VARCHAR(20),
+  service_name VARCHAR(50),
+  id_node_info INT NOT NULL,
   PRIMARY KEY(id)
 );
 
-/* Table 'cpu' */
-CREATE TABLE cpu(
-  id INT NOT NULL AUTO_INCREMENT,
-  cpu_model VARCHAR(50),
-  cpu_mhz INT(50),
-  cpu_core INT(50),
-  id_node_info INT,
-  PRIMARY KEY(id)
-);
-
-/* Table 'user' */
-CREATE TABLE `user`(
-  id INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(20),
-  first_name VARCHAR(20),
-  pwd BINARY(100),
-  email VARCHAR(100),
-  PRIMARY KEY(id)
-);
-
-/* Table 'prix_unitaire_kwh' */
-CREATE TABLE prix_unitaire_kwh(prix_en_euro FLOAT(5));
-
-/* Table 'kwh' */
-CREATE TABLE kwh(
+/* Table 'kwh_service' */
+CREATE TABLE kwh_service(
   id INT NOT NULL AUTO_INCREMENT,
   kwh FLOAT(20),
-  cpu_id INT,
-  PRIMARY KEY(id)
-);
-
-/* Table 'temps' */
-CREATE TABLE temps(
-  id INT NOT NULL AUTO_INCREMENT,
   temps DATETIME,
-  cpu_id INT NOT NULL,
+  service_id INT NOT NULL,
   PRIMARY KEY(id)
 );
 
-/* Relation 'node_info_list_service' */
-ALTER TABLE cpu
-  ADD CONSTRAINT node_info_list_service
-    FOREIGN KEY (id_node_info) REFERENCES node_info (id);
+/* Table 'gpu' */
+CREATE TABLE gpu(
+  id INT NOT NULL AUTO_INCREMENT,
+  gpu_model VARCHAR(50),
+  id_node_info INT NOT NULL,
+  PRIMARY KEY(id)
+);
+
+/* Table 'kwh_gpu' */
+CREATE TABLE kwh_gpu(
+  id INT NOT NULL,
+  kwh FLOAT(20),
+  temps DATETIME,
+  gpu_id INT NOT NULL,
+  PRIMARY KEY(id)
+);
 
 /* Relation 'cpu_kwh' */
 ALTER TABLE kwh ADD CONSTRAINT cpu_kwh FOREIGN KEY (cpu_id) REFERENCES cpu (id);
 
-/* Relation 'cpu_temps' */
-ALTER TABLE temps
-  ADD CONSTRAINT cpu_temps FOREIGN KEY (cpu_id) REFERENCES cpu (id);
+/* Relation 'node_info_service' */
+ALTER TABLE service
+  ADD CONSTRAINT node_info_service
+    FOREIGN KEY (id_node_info) REFERENCES node_info (id);
+
+/* Relation 'service_kwh_service' */
+ALTER TABLE kwh_service
+  ADD CONSTRAINT service_kwh_service
+    FOREIGN KEY (service_id) REFERENCES service (id);
+
+/* Relation 'gpu_kwh_gpu' */
+ALTER TABLE kwh_gpu
+  ADD CONSTRAINT gpu_kwh_gpu FOREIGN KEY (gpu_id) REFERENCES gpu (id);
+
+/* Relation 'node_info_cpu' */
+ALTER TABLE cpu
+  ADD CONSTRAINT node_info_cpu
+    FOREIGN KEY (id_node_info) REFERENCES node_info (id);
+
+/* Relation 'node_info_gpu' */
+ALTER TABLE gpu
+  ADD CONSTRAINT node_info_gpu
+    FOREIGN KEY (id_node_info) REFERENCES node_info (id);
